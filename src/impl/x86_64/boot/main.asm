2bb4c8a2dda7cc9358010c4b55473cc6e2e4c5dc
AndKlet <andreaskletvang@outlook.com>
2026-04-01 09:40:40 +0200

    Update documentation

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -4,5 +4,5 @@ section .text
 bits 32
 start:
   ; print OK
-  mov dword [0xb8000], 0x2f4b2f4f
-  hlt
+  mov dword [0xb8000], 0x2f4b2f4f ; 0xb8000 is the standard physical memory address for the VGA text mode buffer ; 0x2f4b2f4f is "OK"
+  hlt ; halt program after instruction
